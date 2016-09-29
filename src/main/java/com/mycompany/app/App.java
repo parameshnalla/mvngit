0c3e48a545ba754b0dbe1c5b28346200f2e18f06
parameshnalla <sbpraonalla@gmail.com>
2016-09-29 16:00:13 -0400

    Added new syso

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "This is Aman" );
+        System.out.println( "This is Nagma" );
+        System.out.println( "This is Paramesh" );
+
     }
 }
